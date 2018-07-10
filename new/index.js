7412441ebbfbb2ad8b841884846ab9e0faf01c1c
Helen G. Kunda <hgkunda@users.noreply.github.com>
2018-07-10 15:57:50 -0400

    well at least its a slide

--- new/index.js
+++ new/index.js
@@ -1,13 +1,40 @@
-let $ = function(id) {
-	"use strict";
-	window.onload = calculateMpg;
-	return document.getElementById(id);
-};
-
-
 
 $(document).ready(function(){
-  $('.old-proj').slick({
-    // setting-name: setting-value
-  });
+
+$('.slickcaro').slick({
+ltr: true
+  // dots: true,
+  // infinite: false,
+  // speed: 300,
+  // slidesToShow: 3,
+  // slidesToScroll: 3,
+  // responsive: [
+  //   {
+  //     breakpoint: 1024,
+  //     settings: {
+  //       slidesToShow: 3,
+  //       slidesToScroll: 3,
+  //       infinite: true,
+  //       dots: true
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 600,
+  //     settings: {
+  //       slidesToShow: 2,
+  //       slidesToScroll: 2
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 480,
+  //     settings: {
+  //       slidesToShow: 1,
+  //       slidesToScroll: 1
+  //     }
+  //   }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  // ]
+});
 });
