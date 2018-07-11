40a2e97a91d21c476ca63d90511947774f1a1a23
Helen G. Kunda <hgkunda@users.noreply.github.com>
2018-07-11 16:45:27 -0400

    try accordion later

--- new/index.js
+++ new/index.js
@@ -10,4 +10,21 @@ $('.slickautoplay').slick({
   autoplaySpeed: 5000,
 });
 
+$( function() {
+    var icons = {
+      header: "ui-icon-circle-arrow-e",
+      activeHeader: "ui-icon-circle-arrow-s"
+    };
+    $( "#accordion" ).accordion({
+      icons: icons
+    });
+    $( "#toggle" ).button().on( "click", function() {
+      if ( $( "#accordion" ).accordion( "option", "icons" ) ) {
+        $( "#accordion" ).accordion( "option", "icons", null );
+      } else {
+        $( "#accordion" ).accordion( "option", "icons", icons );
+      }
+    });
+  } );
+
 });
