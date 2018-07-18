106a0dfa23c1377eadec37c1d54db0915398bdbe
Helen G. Kunda <hgkunda@users.noreply.github.com>
2018-07-18 15:17:05 -0400

    try again with one top like in example

--- index.js
+++ index.js
@@ -10,47 +10,56 @@ $('.slickautoplay').slick({
   autoplaySpeed: 5000,
 });
 
-$('.slickgala').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3
-  // variableWidth: true
-});
-
 // $('.slickgala').slick({
+//   infinite: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 3
+// });
+
+// let gala = $('.slickgala').slick({
 //   infinite: true,
 //   slidesToShow: 2,
 //   slidesToScroll: 2,
-//   asNavFor: '.slickfinal',
+//   asNavFor: $('.slickfinal')
 //   dots: true,
 //   centerMode: true,
-//   focusOnSelect: true
-// });
-//
-// $('.slickfinal').slick({
-//  slidesToShow: 1,
-//  slidesToScroll: 1,
-//  arrows: false,
-//  fade: true,
-//  asNavFor: '.slickgala'
+//   focusOnSelect: true,
 // });
 
+let final = $('.slickfinal').slick({
+ slidesToShow: 1,
+ slidesToScroll: 1,
+ arrows: false,
+ fade: true,
+ asNavFor: $('.slickgala')
+});
 
-$( function() {
-    var icons = {
-      header: "ui-icon-circle-arrow-e",
-      activeHeader: "ui-icon-circle-arrow-s"
-    };
-    $( "#accordion" ).accordion({
-      icons: icons
-    });
-    $( "#toggle" ).button().on( "click", function() {
-      if ( $( "#accordion" ).accordion( "option", "icons" ) ) {
-        $( "#accordion" ).accordion( "option", "icons", null );
-      } else {
-        $( "#accordion" ).accordion( "option", "icons", icons );
-      }
-    });
-  } );
+let gala = $('.slickgala').slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  asNavFor: $('.slickfinal')
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true,
+});
+
+
+// $( function() {
+//     var icons = {
+//       header: "ui-icon-circle-arrow-e",
+//       activeHeader: "ui-icon-circle-arrow-s"
+//     };
+//     $( "#accordion" ).accordion({
+//       icons: icons
+//     });
+//     $( "#toggle" ).button().on( "click", function() {
+//       if ( $( "#accordion" ).accordion( "option", "icons" ) ) {
+//         $( "#accordion" ).accordion( "option", "icons", null );
+//       } else {
+//         $( "#accordion" ).accordion( "option", "icons", icons );
+//       }
+//     });
+//   } );
 
 });
