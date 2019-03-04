07953665e8d89d76a6b9632dca3e189d64ccdb44
Helen G. Kunda <hgkunda@users.noreply.github.com>
2019-03-04 17:41:13 -0500

    add and keep items

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -11,14 +11,32 @@ class TodoList extends Component {
     this.addItem = this.addItem.bind(this);
     }
     addItem(e) {
+      if (this._inputElement !== " ") {
+        var newItem = {
+          text: this._inputElement.value,
+          key: Date.now()
+        };
 
+        this.setState ( (prevState) => {
+          return {
+            items: prevState.items.concat(newItem)
+          };
+        });
+
+        this._inputElement.value = " ";
+      }
+
+      console.log(this.state.items);
+
+      e.preventDefault();
     }
   render() {
     return (
       <div className="todoListMain">
       <div className="header">
       <form onSubmit={this.addItem}>
-      <input placeholder="enter task">
+      <input ref= { (a) => this._inputElement = a }
+      placeholder="enter task">
       </input>
       <button type="submit">add</button>
 
