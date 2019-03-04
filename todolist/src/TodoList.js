db62697135e037267b35720f4bdbfa093537ff25
Helen G. Kunda <hgkunda@users.noreply.github.com>
2019-03-04 17:36:51 -0500

    working with Todo List JS - before getting to DOM

---
+++ todolist/src/TodoList.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: []
+    };
+
+    this.addItem = this.addItem.bind(this);
+    }
+    addItem(e) {
+
+    }
+  render() {
+    return (
+      <div className="todoListMain">
+      <div className="header">
+      <form onSubmit={this.addItem}>
+      <input placeholder="enter task">
+      </input>
+      <button type="submit">add</button>
+
+      </form>
+      </div></div>
+
+    );
+  }
+}
+
+export default TodoList;
